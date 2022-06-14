4c441b5f832eed53005620e21847ae428f5d985c
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-06-14 18:04:27 +0530

    state

--- github-finder/src/components/layout/Navbar.js
+++ github-finder/src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
+class Navbar extends Component {
     // defualt props
     static defaultProps={
         title :'Github Finder',
