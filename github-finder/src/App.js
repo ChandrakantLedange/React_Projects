582c56f1818db3e44496a6af4dcfe43b8314a152
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-06-14 22:23:12 +0530

    HTTP Request and updating state

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 
 class App extends React.Component {
+  state = {
+    users:[],
+    loading:false
+  }
+  async componentDidMount(){
+    this.setState({loading:true});
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({users:res.data,loading:false});
+    console.log("data:",res.data)
+  }
   render(){
     return (
       <div className="App">
         <Navbar/>
         <div className='container'>
 
-        <Users/>
+        <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
